dd97b42e8f419fd2d1f7fb6175b8842280d3ac79
programmer-zhang <chinajnzhang@hotmail.com>
2020-12-12 21:23:57 +0800

    updata react-app

--- src/index.js
+++ src/index.js
@@ -10,7 +10,13 @@ import './index.css';
 class Square extends React.Component {
     render() {
         return (
-            <button className='square'>
+            <button
+                className='square'
+                onClick={() => {
+                    this.props.onClick();
+                    console.log('点击了');
+                }
+            }>
                 {this.props.value}
             </button>
         );
@@ -18,12 +24,57 @@ class Square extends React.Component {
 }
 
 class Board extends React.Component {
-    renderSquare(i) {
-        return <Square value={i}/>;
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+            xIsNext: true
+        };
+        console.log(this.state.square);
     }
-
+    handleClick(i) {
+        console.log('handleClick', i);
+        const squares = JSON.parse(JSON.stringify(this.state.squares));
+        if (this.calculateWinner(squares) || squares[i]) {
+            return;
+        }
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext
+        });
+    }
+    renderSquare(i) {
+        return <Square
+                    value={this.state.squares[i]}
+                    onClick={() => this.handleClick(i)}
+                />;
+    }
+    calculateWinner(squares) {
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ];
+        for (let i = 0; i < lines.length; i++) {
+            const [a, b, c] = lines[i];
+            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+                return squares[a];
+            }
+        }
+        return null;
+    }
     render() {
-        const status = 'Next player: X';
+        const winner = this.calculateWinner(this.state.squares);
+        let status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        if (winner) {
+            status = 'winner: ' + (!this.state.xIsNext ? 'X' : 'O');
+        }
 
         return (
             <div>
@@ -44,7 +95,7 @@ class Board extends React.Component {
                     {this.renderSquare(8)}
                 </div>
             </div>
-            );
+        );
     }
 }
 
